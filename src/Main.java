9380e9694a45485aa6d16913e9447cc94eb81204
Ziro <Global_counter@mail.ru>
2024-11-24 23:41:40 +0300

    Z1ro <Global_counter@mail.ru>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import classes.Baby;
-import classes.Father;
-import classes.Mood;
-import classes.Room;
+import classes.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +6,7 @@ public class Main {
         Baby b = new Baby("saks",15, Mood.NOTVOSTORG, Room.BEDROOM);
         Baby c = new Baby("boba",15, Mood.GOOD, Room.BEDROOM);
         Father papa = new Father("eblan", 69, Mood.DELIGHTFUL, Room.BEDROOM);
+        Mother mama = new Mother("Захар",228,Mood.TIRED,Room.BEDROOM);
         System.out.println(papa.Search(a));
         a.addFriend(b);
         a.addFriend(c);
